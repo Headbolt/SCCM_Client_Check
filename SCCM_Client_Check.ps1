5a1fb426f9b54e89e8b77c29697b9cbaa2bdb1eb
Headbolt <54707369+Headbolt@users.noreply.github.com>
2022-04-28 12:39:33 +0100

    Create SCCM_Client_Check.ps1

---
+++ SCCM_Client_Check.ps1
@@ -0,0 +1,43 @@
+###############################################################################################################
+#
+# ABOUT THIS PROGRAM
+#
+#   SCCM_Client_Check.ps1
+#   https://github.com/Headbolt/SCCM_Client_Check
+#
+#   This script was designed to Check for the existence of the SCCM Client
+#	and then exit with an appropriate Exit code.
+#
+#	Intended use is in Microsoft Endpoint Manager, as the "Check" half of a Proactive Remediation Script
+#
+###############################################################################################################
+#
+# HISTORY
+#
+#   Version: 1.0 - 28/04/2022
+#
+#   - 28/04/2022 - V1.0 - Created by Headbolt
+#
+###############################################################################################################
+#
+Write-Host
+Write-Host '###############################################################################################################'
+Write-Host
+$Path="c:\windows\ccmsetup\ccmsetup.exe"
+$PathTest=($Path)
+#
+Write-Host 'Checking For File' $Path	
+Write-Host
+Write-Host '###############################################################################################################'
+Write-Host
+#
+If ($PathTest -eq $True)
+{
+	Write-Host 'File' $Path 'Exists'
+	Exit 1
+}
+Else
+{
+	Write-Host 'File' $Path 'Does Not Exist'
+	Exit 0
+}
